0780f3da463a1b05f756d006c7b60765a1e74372
kinjoyuya <kinjo.bobchin@gmail.com>
2021-01-23 23:54:28 +0900

    chore: docker対応の設定追加

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,7 @@ import { FastifyAdapter, NestFastifyApplication } from '@nestjs/platform-fastify
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter());
-  await app.listen(3000);
+  const app = await NestFactory.create<NestFastifyApplication>(AppModule, new FastifyAdapter({ logger: true }));
+  await app.listen(3000, '0.0.0.0');
 }
 bootstrap();
